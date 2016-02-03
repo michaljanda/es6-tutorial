45d2eb3d9b14bd5a2e309e14914726f452051152
jandam <michal.janda@acision.com>
2016-02-03 16:22:20 +0100

    round 2 using let

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 var calculateMonthlyPayment = function (principal, years, rate) {
     if (rate) {
-        var monthlyRate = rate / 100 / 12;
+        let monthlyRate = rate / 100 / 12;
     }
-    var monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1 / (1 + monthlyRate), years * 12)));
+    let monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1 / (1 + monthlyRate), years * 12)));
     return monthlyPayment;
 };
 
